4ec54abedb82f56c39710d7255837fa12d1d7d5e
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-11-11 11:10:11 -0800

    changed main

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/pj-react-777">
+        <BrowserRouter basename="/power-pulse">
           <ThemeProvider theme={glodalColor}>
             <GlobalStyle />
             <App />
