4ee05928725a36fd52127a788783f445f8ee6966
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-12-11 12:21:37 -0800

    updated the styling for the user icon

--- src/components/UserCards/UserCards.styled.jsx
+++ src/components/UserCards/UserCards.styled.jsx
@@ -197,9 +197,6 @@ export const LogoutButton = styled.button`
 `;
 
 export const Icon = styled.svg`
-  position: absolute;
-  top: 14px;
-  right: 14px;
   pointer-events: none;
   &.orange {
     fill: ${glodalColor.secondaryOrange};
